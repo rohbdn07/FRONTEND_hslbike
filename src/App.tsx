9ac290d4ebb2fdb7f4c75bd89c40d2a69f958ada
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-08-08 18:47:12 +0300

    eslint formatted

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
+import './App.css'
+import MenuBar from './components/navbar/Menubar'
+import DataTableList from './components/table/DataTable'
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h2>HSL CITY BIKE DASHBOARD</h2>
-            </header>
+  return (
+    <>
+      <div className='App'>
+        <div className='MenuBar'>
+          <MenuBar />
         </div>
-    );
+        <div className='DataTable'>
+          <DataTableList />
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
