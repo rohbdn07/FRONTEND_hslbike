bb6d8ae64a4dab48a063edcc3e4ad27f0ff6f902
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-08-12 00:18:27 +0300

    added new routes to display pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom'
 import JourneyPage from '../pages/JourneyPage'
+import StationByIdPage from '../pages/StationByIdPage'
 import StationPage from '../pages/StationPage'
 
+/**
+ *
+ * @returns various routes to display pages
+ */
 const AppRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path='/journeylist' element={<JourneyPage />} />
         <Route path='/' element={<Navigate replace to='/journeylist' />} />
+        <Route path='/journeylist' element={<JourneyPage />} />
         <Route path='/stationlist' element={<StationPage />} />
+        <Route path='/stationlist/:id' element={<StationByIdPage />} />
+        <Route
+          path='*'
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>Opps! this route doesnot exist. There is nothing here!</p>
+            </main>
+          }
+        />
       </Routes>
     </>
   )
