2cf766d1c9013a8662c2d42d6421a340a7705fae
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-08-12 00:12:37 +0300

    put description and rename menubar to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import './App.css'
-import MenuBar from './components/navbar/Menubar'
+import NavBar from './components/navbar/NavBar'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes'
 
+/**
+ * @description this component is the main entry point to various routes
+ * @returns JSX Elements with browser router
+ */
 function App() {
   return (
     <>
       <BrowserRouter>
         <div className='App'>
           <div className='MenuBar'>
-            <MenuBar />
+            <NavBar />
           </div>
           <AppRoutes />
         </div>
