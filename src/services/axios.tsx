c2e4135c1a595e06029df7a9ce7954e8461689ef
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-08-12 00:19:18 +0300

    added: a function to call api endpoint

--- src/services/axios.tsx
+++ src/services/axios.tsx
@@ -4,10 +4,21 @@ import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
  * @baseurl "http://localhost:5050"
  */
 
-export const axiosFetchAuthAPI: AxiosInstance & AxiosRequestConfig = axios.create({
-  // baseURL: "http://localhost:5050",
+const axiosFetchStationInfoByIdAPI: AxiosInstance & AxiosRequestConfig = axios.create({
+  // baseURL: 'http://localhost:5050',
   baseURL: process.env.REACT_APP_HOST || 'http://localhost:5050',
   headers: {
     'Content-Type': 'application/json',
   },
 })
+
+// call an api endpoint and return response
+const getStationInfoById = async (id: string) => {
+  const response = await axiosFetchStationInfoByIdAPI.get(`/api/hslcitybike/stationsinfo/${id}/may`)
+  return response
+}
+
+export default {
+  axiosFetchStationInfoByIdAPI,
+  getStationInfoById,
+}
