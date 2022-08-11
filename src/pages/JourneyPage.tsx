951613c94ed974e45afdce3c602e897fdbf03c2e
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-08-11 12:47:30 +0300

    created: a seperate pages for listing tables

---
+++ src/pages/JourneyPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DataTableList from '../components/table/DataTable'
+
+const JourneyPage = () => {
+  return (
+    <>
+      <div className='DataTable'>
+        <DataTableList />
+      </div>
+    </>
+  )
+}
+
+export default JourneyPage
---
+++ src/pages/StationPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DataTableStationList from '../components/table/DataTableStation'
+
+const StationPage = () => {
+  return (
+    <>
+      <div className='DataTable'>
+        <DataTableStationList />
+      </div>
+    </>
+  )
+}
+
+export default StationPage
