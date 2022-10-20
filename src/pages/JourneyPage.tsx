f5fe5297685d4bfee38b5a6d2dd80571836cc71e
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-10-20 23:56:17 +0300

    refactor code by using custom hook for api call

--- src/pages/JourneyPage.tsx
+++ src/pages/JourneyPage.tsx
@@ -1,37 +1,26 @@
 import { Box } from '@mui/material'
 import React from 'react'
+
 import Header from '../components/headers/Header'
 import DataTableList from '../components/table/DataTable'
-import axiosServices from '../services/axios'
+import { useCallApiHook } from '../hooks/useCallApiHook'
 
 /**
  * @description this component display journey list of data
  * @returns JSX Element
  */
 const JourneyPage = () => {
-  const [data, setData] = React.useState([])
-  const [dataNotFound, setDataNotFound] = React.useState<null>(null)
-
-  // call axios services to fetch data
-  const callApi = async (id: string, value: string) => {
-    try {
-      const response = await axiosServices.getAllJourneyList(value as string)
-      const data = await response.data
-      if (data.success === true) {
-        setData(data)
-        setDataNotFound(null)
-      } else if (data.success === false) {
-        setDataNotFound(data.message)
-      }
-    } catch (error) {
-      console.log('Error while fetching..', error)
-    }
-  }
+  // this custom hook call an api and return required states
+  const [data, dataNotFound, selectedMonth, setSelectedMonth] = useCallApiHook('journeyList')
 
   return (
     <>
       <div className='DataTable'>
-        <Header header={'Journey List'} fetchAPIFn={callApi} />
+        <Header
+          header={'Journey List'}
+          selectedMonth={selectedMonth}
+          setSelectedMonth={setSelectedMonth}
+        />
         {dataNotFound !== null ? (
           <Box sx={{ fontSize: '20px', color: 'white', marginTop: '20px' }}>{dataNotFound}</Box>
         ) : (
